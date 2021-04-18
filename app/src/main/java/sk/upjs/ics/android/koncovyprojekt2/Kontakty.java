d626830e4abd773b94c1f358c75fe91ef0ddb903
MargaretaRenckova <potvorka9@gmail.com>
2021-04-18 15:20:43 +0200

    Na kontaktoch po kliknutí na číslo presunutie do aplikácie kontakty v telefóne, pridanie predčíslia +421 pred telefónne čísla v prípade že je volajúci v zahraničí (čo sa môže stať), opravenie scrollingu v fragment_karantena.xml

--- app/src/main/java/sk/upjs/ics/android/koncovyprojekt2/Kontakty.java
+++ app/src/main/java/sk/upjs/ics/android/koncovyprojekt2/Kontakty.java
@@ -13,10 +13,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
 public class Kontakty extends Fragment {
-
-
+    private TextView bratislava;
+    private TextView kosice;
+    private TextView trnava;
+    private TextView trencin;
+    private TextView nitra;
+    private TextView zilina;
+    private TextView banskaBystrica;
+    private TextView presov;
 
     public Kontakty() {
         // Required empty public constructor
@@ -35,7 +42,36 @@ public class Kontakty extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_kontakty, container, false);
+        View frameLayout = inflater.inflate(R.layout.fragment_kontakty, container, false);
+        bratislava = frameLayout.findViewById(R.id.bratislava);
+        bratislava.setOnClickListener(makeCall(bratislava));
+        kosice = frameLayout.findViewById(R.id.kosice);
+        kosice.setOnClickListener(makeCall(kosice));
+        trnava = frameLayout.findViewById(R.id.trnava);
+        trnava.setOnClickListener(makeCall(trnava));
+        trencin = frameLayout.findViewById(R.id.trencin);
+        trencin.setOnClickListener(makeCall(trencin));
+        nitra = frameLayout.findViewById(R.id.nitra);
+        nitra.setOnClickListener(makeCall(nitra));
+        zilina = frameLayout.findViewById(R.id.zilina);
+        zilina.setOnClickListener(makeCall(zilina));
+        banskaBystrica = frameLayout.findViewById(R.id.bansbystrica);
+        banskaBystrica.setOnClickListener(makeCall(banskaBystrica));
+        presov = frameLayout.findViewById(R.id.presov);
+        presov.setOnClickListener(makeCall(presov));
+        return frameLayout;
+    }
+
+    public View.OnClickListener makeCall(TextView number){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:" + number.getText()));
+                startActivity(callIntent);
+            }
+        };
+
     }
 
 
